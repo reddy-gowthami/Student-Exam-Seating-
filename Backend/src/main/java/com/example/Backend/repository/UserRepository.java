ce92d5e1a3303d464a3d075fdb478e5bf08c05b9
reddy-gowthami <gowthamireddem@gmail.com>
2026-04-20 13:41:37 +0530

    admin role code is added1

--- Backend/src/main/java/com/example/Backend/repository/UserRepository.java
+++ Backend/src/main/java/com/example/Backend/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.Backend.entity.User;
 import com.example.Backend.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -12,11 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByRole(Role role);
 
     // Find user by email (for login)
-<<<<<<< HEAD
     User findByEmail(String email);
 
     long countByRole(Role role);
-=======
-    Optional<User> findByEmail(String email);
->>>>>>> 5a46ab4eb2fae2ce31d282772a60f8ccbad32735
 }
