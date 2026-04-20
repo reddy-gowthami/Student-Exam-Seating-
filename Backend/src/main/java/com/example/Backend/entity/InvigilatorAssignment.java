10da1ac839f06967f24512e424dbba5a0ebee0fa
sravani029 <sravanimekapotula@gmail.com>
2026-04-20 12:11:11 +0530

    entity validation

--- Backend/src/main/java/com/example/Backend/entity/InvigilatorAssignment.java
+++ Backend/src/main/java/com/example/Backend/entity/InvigilatorAssignment.java
@@ -2,6 +2,7 @@ package com.example.Backend.entity;
 
 import com.example.Backend.enums.AssignmentStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -15,18 +16,22 @@ public class InvigilatorAssignment{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name="exam_id",nullable=false)
     private Exam exam;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name="faculty_id",nullable=false)
     private User faculty;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name="hall_id",nullable=false)
     private ExamHall examHall;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private AssignmentStatus assignmentStatus=AssignmentStatus.ASSIGNED;
 
